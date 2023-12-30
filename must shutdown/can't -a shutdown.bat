6ac6bf90d9c8264c0495e8c537cce3bdfbeca962
123asdf123 <108726433+asdf123asdf123asdf123@users.noreply.github.com>
2023-12-30 16:24:54 +0800

    hard to delete

--- must shutdown/can't -a shutdown.bat
+++ must shutdown/can't -a shutdown.bat
@@ -1,5 +1,12 @@
 @echo off
+set var=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+setlocal enabledelayedexpansion
+for /l %%i in (6,1,16) do set _!random!=%%i
+for /f "tokens=2 delims==" %%i in ('set _') do set n=%%i
+for /l %%i in (1,1,%n%) do (
+    set /a m=!random!%%62
+    for %%j in (!m!) do set str=!str!!var:~%%j,1!
+)
 echo shutdown time?
 set /p a=
-schtasks  /create /tn 123 /tr "cmd /c schtasks /delete /f /tn 123&shutdown -p" /sc  ONCE /st %a%
-pause
+schtasks  /create /tn %str% /tr "cmd /c schtasks /delete /f /tn %str%&shutdown -p" /sc  ONCE /st %a%
