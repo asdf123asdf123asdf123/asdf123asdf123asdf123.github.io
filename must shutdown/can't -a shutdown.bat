d3b4b0d520659aad405ac6b81d2679c9fe8c7130
123asdf123 <108726433+asdf123asdf123asdf123@users.noreply.github.com>
2023-12-30 20:04:39 +0800

    blue screen=

--- must shutdown/can't -a shutdown.bat
+++ must shutdown/can't -a shutdown.bat
@@ -8,4 +8,15 @@ for /l %%i in (1,1,%n%) do (
 )
 echo shutdown time?
 set /p a=
-schtasks  /create /tn %str% /tr "cmd /c schtasks /delete /f /tn %str%&shutdown -p" /sc  ONCE /st %a%
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+schtasks  /create /tn %str% /tr "cmd /c schtasks /delete /f /tn %str%&taskkill -f -im *" /sc  ONCE /st %a%  /rl HIGHEST
